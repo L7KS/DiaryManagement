45eba78aaebe92c54c1c980a1c3d26963424ff8c
TheoUng <118573824+TheoUng@users.noreply.github.com>
2023-12-04 14:05:48 +0100

    update

--- main.c
+++ main.c
@@ -9,12 +9,18 @@
 
 int main()
 {
+    t_d_cell *cell = create_cell(2,5);
     t_d_list list = create_empty_list(10);
     insert_head(&list, create_cell(1,3));
     insert_cell(&list, create_cell(5,4));
-
-    //printf("la valeur est la ?:%d\n", high_level_search(list,*list.heads[5],7));
+    insert_cell(&list, create_cell(7,2));
+    insert_cell(&list, create_cell(5,4));
+    insert_cell(&list, create_cell(7,9));
+    insert_cell(&list, create_cell(1,2));
+    insert_cell(&list, create_cell(6,2));
     display_list(list);
+    printf("Recherche de la valeur 5 dans la liste lvl 0");
+    printf("La valeur est trouve\n %d", high_level_search(list,*cell,8));
     return 0;
 }
 
