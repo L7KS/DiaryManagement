b28333f75653942dadd52825b361959561993c37
Sakutaa <jonathan.rajoeliarivony@gmail.com>
2023-12-10 19:56:49 +0100

    visuel

--- main.c
+++ main.c
@@ -6,30 +6,46 @@
 #include <stdlib.h>
 #include "list.h"
 #include "cell.h"
+#include "main.h"
+
 int main()
 {
     printf("Premiere Partie :\n");
     printf("Creer une liste vide: \n");
     t_d_list list = create_empty_list(9);
     display_list(list);
+    line();
+
     printf("Ajouter en tete de liste:\n");
     t_d_list list1 = create_empty_list(3);
     insert_head(&list1, create_cell(1,2));
     display_list(list1);
+    line();
+
     printf("Ajouter une cellule dans la liste 1:\n");
     insert_cell(&list1, create_cell(3, 1));
     display_list(list1);
+    line();
 
     int value;
     printf("Partie 2: \n");
-    printf("Creation d'une liste 2^n-1: \n ");
+    printf("Creation d'une liste 2^n-1: \n");
     t_d_list list2 = create_list(3);
     display_list(list2);
+    line();
+
     printf("Recherche d'une valeur lvl 0 :\n");
+    printf("\n");
+
     printf("La valeur est trouve %d", level_0_search(list2, 3));
+    line();
+
     printf("\nRecherche d'une valeur tous les niveaux :");
+    printf("\n");
     value= high_level_search(list2, list2.heads[4-1], 8);
     printf("\nLa valeur est trouve %d :",value);
+    line();
+
     return 0;
 }
 
--- main.h
+++ main.h
@@ -5,4 +5,12 @@
 #ifndef DIARYMANAGEMENT_MAIN_H
 #define DIARYMANAGEMENT_MAIN_H
 
+#include <stdio.h>
+
+void line(){
+    printf("\n");
+    printf("-----------------------------------------\n");
+    printf("\n");
+}
+
 #endif //DIARYMANAGEMENT_MAIN_H
